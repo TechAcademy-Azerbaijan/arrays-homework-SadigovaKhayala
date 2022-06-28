538ddaf8d7cd6ada0ecc977b090352dda736dea0
SadigovaKhayala <106319208+SadigovaKhayala@users.noreply.github.com>
2022-06-28 14:34:20 +0400

    Update main.js

--- task-4/main.js
+++ task-4/main.js
@@ -1,9 +1,22 @@
-const prompt = require('prompt');
-prompt.start();
+let arr=[14,16,3,7,17,19,9];
+let narr=[];
+let b=0;
+
+for(i=1;i<arr.length;i++){
 
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+if(arr[i]>arr[i-1]){
+narr.push(arr[i]);
+}
+
+else{narr.push(arr[i-1])}
+}
+
+
+for(i=0;i<narr.length;i++){
+    if(narr[i]==narr[i+1]){
+        narr.splice(narr[i],1);
+    }
+}
+
+console.log(narr)
