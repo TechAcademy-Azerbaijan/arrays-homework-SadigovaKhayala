7879e512c0d64250128b931f923630b026e68ac7
SadigovaKhayala <106319208+SadigovaKhayala@users.noreply.github.com>
2022-06-28 19:23:56 +0400

    Update main.js

--- task-4/main.js
+++ task-4/main.js
@@ -1,3 +1,5 @@
+
+
 let arr=[14,16,3,7,17,19,9];
 let narr=[];
 let b=0;
@@ -7,16 +9,7 @@ for(i=1;i<arr.length;i++){
 
 if(arr[i]>arr[i-1]){
 narr.push(arr[i]);
-}
-
-else{narr.push(arr[i-1])}
-}
+}}
 
 
-for(i=0;i<narr.length;i++){
-    if(narr[i]==narr[i+1]){
-        narr.splice(narr[i],1);
-    }
-}
-
-console.log(narr)
+console.log(narr.join(' '));
