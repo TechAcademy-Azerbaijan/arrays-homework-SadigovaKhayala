109b98677d82a7127bd5c8c7dd85f68474a64fee
SadigovaKhayala <106319208+SadigovaKhayala@users.noreply.github.com>
2022-06-28 17:11:57 +0400

    Update main.js

--- task-8/main.js
+++ task-8/main.js
@@ -1,9 +1,21 @@
-const prompt = require('prompt');
-prompt.start();
+let  arr=[6,7, -3, 5 ,-5 ,-4, 7 ,-5];
+let max=0;
+let b;
+let temp=arr[arr.length-1]
+for(i=0;i<arr.length;i++){
+    if(max<arr[i]){
+        max=arr[i]
+    }
 
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+else if(arr[i]==max){
+  b=i;
+
+}
+}
+
+arr[b]=temp;
+arr[arr.length-1]=max
+
+
+console.log(arr);
