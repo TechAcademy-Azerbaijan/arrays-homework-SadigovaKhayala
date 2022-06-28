2fb0a3b86319affdee74dc0ec6a976f4c91f224d
SadigovaKhayala <106319208+SadigovaKhayala@users.noreply.github.com>
2022-06-28 15:59:39 +0400

    Update main.js

--- task-5/main.js
+++ task-5/main.js
@@ -1,9 +1,17 @@
-const prompt = require('prompt');
-prompt.start();
-
-
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+let arr=[1,2,3,4,5,6,7];
+let last=arr[arr.length-1]
+let narr=[]
+if(arr.length%2==0){
+for(i=0;i<arr.length;i=i+2){
+narr.push(arr[i+1]);
+narr.push(arr[i])
+}
+}
+else{
+    for(i=0;i<arr.length-1;i=i+2){
+        narr.push(arr[i+1]);
+        narr.push(arr[i])
+        }
+    narr.push(last)
+}
+console.log(narr)
