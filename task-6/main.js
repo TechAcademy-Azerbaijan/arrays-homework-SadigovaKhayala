c0c7d65a8cb32216975aed4c7164b39ed0794265
SadigovaKhayala <106319208+SadigovaKhayala@users.noreply.github.com>
2022-06-28 16:12:09 +0400

    Update main.js

--- task-6/main.js
+++ task-6/main.js
@@ -1,9 +1,19 @@
-const prompt = require('prompt');
-prompt.start();
+let  arr=[1,2,6,4];
+let min=100;
+let max=0;
+let sum=0
+
+for(let i=0;i<arr.length;i++){
+    if(arr[i]>max){
+        max=arr[i]
+    }
+
+    if (arr[i]<min){
+        min=arr[i]
+    }
+sum=sum+arr[i]
+
+}
 
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+console.log(sum-max-min)
