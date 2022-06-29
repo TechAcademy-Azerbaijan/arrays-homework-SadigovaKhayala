3395a9d750436da8151fcd9a833fe034c7530f53
SadigovaKhayala <106319208+SadigovaKhayala@users.noreply.github.com>
2022-06-29 12:48:47 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -1,13 +1,10 @@
-let  arr=[14,16,3,7,17,19,9];
-let a=0;
-
-
+let arr=[14, 16, 3, 7, 17, 19, 9,];
+let count=0
+let temp=[]
 
 for(i=0;i<arr.length;i++){
-if(i%2==0){
-   a=arr[i]
- 
-console.log(a)
-
+    if(i%2===0){
+        temp.push(arr[i])
+    }
 }
-}
+    console.log(temp)
