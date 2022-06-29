beb28d33da1cc1b5a43c9b2c13fee3e44cbee5cf
SadigovaKhayala <106319208+SadigovaKhayala@users.noreply.github.com>
2022-06-29 12:51:58 +0400

    Update main.js

--- task-4/main.js
+++ task-4/main.js
@@ -1,15 +1,10 @@
+let arr=[14, 16, 3, 7, 17, 19, 9];
+let count=0
+let temp=[]
 
-
-let arr=[14,16,3,7,17,19,9];
-let narr=[];
-let b=0;
-
-for(i=1;i<arr.length;i++){
-
-
-if(arr[i]>arr[i-1]){
-narr.push(arr[i]);
-}}
-
-
-console.log(narr.join(' '));
+for(i=0;i<arr.length;i++){
+    if(arr[i]>arr[i-1]){
+        temp.push(arr[i])
+    }
+}
+    console.log(temp)
